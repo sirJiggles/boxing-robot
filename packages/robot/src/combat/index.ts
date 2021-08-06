8cd7b64a81be85c5538a8fa36889c6e389c530bb
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2021-08-06 18:39:51 +0200

    stop le bashing of the buttons, might move this to somewhere else

--- packages/robot/src/combat/index.ts
+++ packages/robot/src/combat/index.ts
@@ -1,9 +1,20 @@
 import { back, out } from '../servo'
 
+// state for what is out and in, start up all up
+const armsOut = [false, false, false, false]
+
 export const doHit = (arm: number) => {
   // go out and back in again
+  // cannot go out if already out
+  if (armsOut[arm]) {
+    return
+  }
   out(arm)
+  armsOut[arm] = true
   setTimeout(() => {
-    back(arm)
+    if (!armsOut[arm]) {
+      back(arm)
+      armsOut[arm] = false
+    }
   }, 1000)
 }
