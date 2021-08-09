f7b2c9e17e4449b975a581d3fb5c7900be2ed0d6
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2021-08-10 00:06:15 +0200

    update le cron

--- packages/robot/cron.sh
+++ packages/robot/cron.sh
@@ -1,6 +1,8 @@
 echo "hello"
-sleep 5
-cd /home/pi/Desktop/development/boxing-robot/packages/robot
+sleep 10
+cd /home/pi/Desktop/development/boxing-robot/
+git pull
+cd ./packages/robot
 sudo yarn build
 cp .env ./dist
 sudo /home/pi/.config/versions/node/v16.6.1/bin/forever start ./dist/index.js
