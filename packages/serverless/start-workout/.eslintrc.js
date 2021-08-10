d2f19a124aa6722740ad596d0ecd5d48a1370ef5
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2021-08-10 23:39:57 +0200

    remove all unused code

--- packages/serverless/start-workout/.eslintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: ['@boxing-robot/eslint-config'],
-}
--- packages/serverless/start-workout/cors-headers.ts
+++
@@ -1,7 +0,0 @@
-const corsHeaders = {
-  'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'OPTIONS',
-}
-
-export { corsHeaders }
